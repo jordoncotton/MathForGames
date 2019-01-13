bf0ab076c7fba1131a277a27d0de60b1da12da22
jordoncotton1 <jordoncotton1@gmail.com>
2019-01-13 16:20:37 -0600

    matrix

--- MathForGames/Matrix3.cpp
+++ MathForGames/Matrix3.cpp
@@ -2,21 +2,56 @@
 
 matrix3::matrix3()
 {
-	for (int i = 0; i <= 2; i++)
+	for (int i = 0; i < 3; i++)
 	{
-		for (int l = 0; l <= 2; l++)
+		for (int l = 0; l < 3; l++)
 		{
-			num[i][l] = 0.0;
+			matrix[i][l] = 0.0;
 		}
+	}
+}
+
+matrix3::matrix3(Vector3 Xvec, Vector3 Yvec, Vector3 Zvec)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		matrix[i][0] = Xvec[i];
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		matrix[i][1] = Yvec[i];
+	}
+	
+	for (int i = 0; i < 3; i++)
+	{
+		matrix[i][2] = Zvec[i];
 	}
 }
 
 void matrix3::set(matrix3)
 {
-
+	
 }
 
 matrix3 matrix3::operator*(const matrix3) const
 {
 	return matrix3();
+}
+
+Vector3 matrix3::operator*(const Vector3) const
+{
+	return Vector3();
+}
+
+void matrix3::rotateX(float)
+{
+}
+
+void matrix3::rotateY(float)
+{
+}
+
+void matrix3::rotateZ(float)
+{
 }
--- MathForGames/Matrix3.h
+++ MathForGames/Matrix3.h
@@ -5,14 +5,23 @@ class matrix3
 {
 public:
 	matrix3();
+	matrix3(Vector3, Vector3, Vector3);
+
 	void set(matrix3);
 	matrix3 operator*(const matrix3)const;
+	Vector3 operator*(const Vector3)const;
+
+	void rotateX(float);
+	void rotateY(float);
+	void rotateZ(float);
 
 private:
+
+	float matrix[3][3];
+
 	Vector3 xcol;
 	Vector3 ycol;
 	Vector3 zcol;
-
 	Vector3 xrow;
 	Vector3 yrow;
 	Vector3 zrow;
